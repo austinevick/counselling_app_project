729bb206e36be0de6d7135c62dd8f1f96e37e576
Augustine Victor <augustinevickky@gmail.com>
2021-01-22 20:48:03 +0100

    Update email_sign_in.dart

--- lib/screen/email_sign_in.dart
+++ lib/screen/email_sign_in.dart
@@ -1,7 +1,23 @@
+import 'package:counselling_app_project/services/authentication.dart';
 import 'package:counselling_app_project/widget/login_button.dart';
 import 'package:flutter/material.dart';
 
-class EmailSignInPage extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
+
+class EmailSignInPage extends StatefulWidget {
+  @override
+  _EmailSignInPageState createState() => _EmailSignInPageState();
+}
+
+class _EmailSignInPageState extends State<EmailSignInPage> {
+  final emailController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  EmailSignInFormType formType = EmailSignInFormType.signIn;
+  String btnText() =>
+      formType == EmailSignInFormType.signIn ? 'LOGIN' : 'REGISTER';
+  String secondaryText() => formType == EmailSignInFormType.signIn
+      ? 'Need an account? Register'
+      : 'Have an account? Login';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,16 +40,27 @@ class EmailSignInPage extends StatelessWidget {
                   height: 15,
                 ),
                 LoginButtons(
-                  text: 'LOGIN',
-                  onPressed: () {},
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Need an account?'),
-                    FlatButton(onPressed: () {}, child: Text('Register')),
-                  ],
-                )
+                    text: btnText(),
+                    onPressed: () {
+                      if (formType == EmailSignInFormType.signIn) {
+                        AuthenticationService.signInWithEmailAndPassword(
+                            emailController.text, passwordController.text);
+                      } else {
+                        AuthenticationService.createUserWithEmailAndPassword(
+                            emailController.text, passwordController.text);
+                      }
+                    }),
+                FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        formType = formType == EmailSignInFormType.signIn
+                            ? EmailSignInFormType.register
+                            : EmailSignInFormType.signIn;
+                        emailController.clear();
+                        passwordController.clear();
+                      });
+                    },
+                    child: Text(secondaryText()))
               ],
             ),
           ),
