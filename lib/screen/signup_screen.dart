eb1466f78777a13a9f496ce6300ca2eb68b580b7
Augustine Victor <augustinevickky@gmail.com>
2021-03-15 07:35:07 +0100

    Update signup_screen.dart

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -124,8 +124,8 @@ class _SignupScreenState extends State<SignupScreen> {
   }
 
   String validateMobile(String value) {
-    String patttern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
-    RegExp regExp = new RegExp(patttern);
+    String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+    RegExp regExp = new RegExp(pattern);
     if (value.length == 0) {
       return 'Please enter mobile number';
     } else if (!regExp.hasMatch(value)) {
